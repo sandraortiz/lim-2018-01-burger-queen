8c1d35b596bbecbc41bfb3af45398e3034431bd7
sandra <sandrarocio12oc@gmail.com>
2019-04-07 22:57:32 -0500

    Updates

---
+++ precache-manifest.5288630027d5898db0fb1bc1de4070c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "522e57584f5add9df045",
+    "url": "/sandraortiz/lim-2018-01-burger-queen/static/js/runtime~main.4ef198ad.js"
+  },
+  {
+    "revision": "35c8c397e9817956bbb1",
+    "url": "/sandraortiz/lim-2018-01-burger-queen/static/js/main.88e99a81.chunk.js"
+  },
+  {
+    "revision": "86cb1bcd40321bb63858",
+    "url": "/sandraortiz/lim-2018-01-burger-queen/static/js/2.49c0c782.chunk.js"
+  },
+  {
+    "revision": "35c8c397e9817956bbb1",
+    "url": "/sandraortiz/lim-2018-01-burger-queen/static/css/main.89f6943b.chunk.css"
+  },
+  {
+    "revision": "e573c525c2f2587744655e71f69cd5f8",
+    "url": "/sandraortiz/lim-2018-01-burger-queen/index.html"
+  }
+];
